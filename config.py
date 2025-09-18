ad6527cee5d7cdab6e431f9252023ef2150aa97a
elzahraku <malikelza9@gmail.com>
2025-09-18 15:40:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/navycodes/meplugins",
+    "https://github.com/elzahraku/JOOX",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
